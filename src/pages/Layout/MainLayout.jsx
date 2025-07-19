a75d2eb0d56c59aa10a426ef7cd4db1377bd9a37
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-07-19 20:16:46 +0600

    about building

--- src/pages/Layout/MainLayout.jsx
+++ src/pages/Layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Home/Banner';
+import AboutBuilding from '../Home/AboutBuilding';
 
 const MainLayout = () => {
     return (
         <div>
             <Banner></Banner>
+            <AboutBuilding></AboutBuilding>
             
         </div>
     );
