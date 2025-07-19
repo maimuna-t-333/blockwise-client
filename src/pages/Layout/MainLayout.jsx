f35800aa961866ba72c4a6a60d9820bb612348cf
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-07-19 20:55:06 +0600

    map

--- src/pages/Layout/MainLayout.jsx
+++ src/pages/Layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Home/Banner';
 import AboutBuilding from '../Home/AboutBuilding';
+import LocationDetails from '../Home/LocationDetails';
 
 const MainLayout = () => {
     return (
         <div>
             <Banner></Banner>
             <AboutBuilding></AboutBuilding>
+            <LocationDetails></LocationDetails>
             
         </div>
     );
