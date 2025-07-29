d6b511bd4c7cd27b015ea1632c3934a1e3356a29
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-07-29 23:13:51 +0600

    payment method applied

--- src/pages/DashBoard/MakePayment.jsx
+++ src/pages/DashBoard/MakePayment.jsx
@@ -1,8 +1,18 @@
+import StripeProvider from "../../context/StripeProvider";
+import CheckoutForm from "./CheckoutForm";
+
 const MakePayment = () => {
+
+  const amount = 1000;
+
   return (
     <div className="p-6 bg-white rounded shadow-md max-w-xl mx-auto">
       <h2 className="text-xl font-bold mb-4">Make Payment</h2>
-      <p className="text-gray-600">Stripe payment form will be implemented here.</p>
+      <StripeProvider>
+        <CheckoutForm
+          amount={amount}
+        ></CheckoutForm>
+      </StripeProvider>
     </div>
   );
 };
