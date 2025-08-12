ee572ae6002b75909a264e475a2f69bbcba8196d
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-08-12 15:38:46 +0600

    apartment card

--- src/pages/Layout/MainLayout.jsx
+++ src/pages/Layout/MainLayout.jsx
@@ -3,12 +3,14 @@ import Banner from '../Home/Banner';
 import AboutBuilding from '../Home/AboutBuilding';
 import LocationDetails from '../Home/LocationDetails';
 import CouponSection from '../Home/CouponSection';
+import ApartmentSection from '../Home/ApartmentSection';
 
 const MainLayout = () => {
     return (
         <div>
             <Banner></Banner>
             <AboutBuilding></AboutBuilding>
+            <ApartmentSection></ApartmentSection>
             <LocationDetails></LocationDetails>
             <CouponSection></CouponSection>
             
