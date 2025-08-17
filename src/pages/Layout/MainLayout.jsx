7fe713a0e07b1da5fce3c148d5d3c3accafb49da
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-08-18 03:39:20 +0600

    about us

--- src/pages/Layout/MainLayout.jsx
+++ src/pages/Layout/MainLayout.jsx
@@ -4,12 +4,15 @@ import AboutBuilding from '../Home/AboutBuilding';
 import LocationDetails from '../Home/LocationDetails';
 import CouponSection from '../Home/CouponSection';
 import ApartmentSection from '../Home/ApartmentSection';
+import AboutUs from '../Home/AboutUs';
 
 const MainLayout = () => {
     return (
         <div>
             <Banner></Banner>
+            
             <AboutBuilding></AboutBuilding>
+            <AboutUs></AboutUs>
             <ApartmentSection></ApartmentSection>
             <LocationDetails></LocationDetails>
             <CouponSection></CouponSection>
